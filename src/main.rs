a2921cdeab463da2240f682a29597acd808ffaf2
umaumax <umauma7083@gmail.com>
2022-02-23 21:08:06 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: update logs
     eprintln!("start server {:?}", listener);
     loop {
-        let (mut client_socket, _) = listener.accept().await?;
+        let (mut client_stream, _) = listener.accept().await?;
 
         tokio::spawn(async move {
             eprintln!("connect from xxx");
             let server_addr = "localhost:22222";
-            let mut server_socket = TcpStream::connect(server_addr.clone()).await.unwrap();
+            let mut server_stream = TcpStream::connect(server_addr.clone()).await.unwrap();
             eprintln!("connect to {}", server_addr);
 
             let mut outbound_buf = [0; BUFFER_SIZE];
@@ -26,42 +26,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             loop {
                 tokio::select! {
                     // send(outbound)
-                    n = client_socket.read(&mut outbound_buf) =>{
+                    n = client_stream.read(&mut outbound_buf) =>{
                         let n = match n{
                             Ok(n) if n == 0 => return,
                             Ok(n) => n,
                             Err(e) => {
-                                eprintln!("failed to read from socket; err = {:?}", e);
+                                eprintln!("failed to read from stream; err = {:?}", e);
                                 return;
                             }
                         };
                         eprintln!("-receive-> {} bytes", n);
 
-                        match server_socket.write_all(&outbound_buf[0..n]).await {
+                        match server_stream.write_all(&outbound_buf[0..n]).await {
                             Ok(_n) => {}
                             Err(e) => {
-                                eprintln!("failed to write to socket; err = {:?}", e);
+                                eprintln!("failed to write to stream; err = {:?}", e);
                                 return;
                             }
                         }
                         eprintln!("-send-> {} bytes", n);
                     }
                     // receive(inbound)
-                    n = server_socket.read(&mut inbound_buf) =>{
+                    n = server_stream.read(&mut inbound_buf) =>{
                         let n=match n {
                             Ok(n) if n == 0 => return,
                             Ok(n) => n,
                             Err(e) => {
-                                eprintln!("failed to read from socket; err = {:?}", e);
+                                eprintln!("failed to read from stream; err = {:?}", e);
                                 return;
                             }
                         };
                         eprintln!("<-receive- {} bytes", n);
 
-                        match client_socket.write_all(&inbound_buf[0..n]).await {
+                        match client_stream.write_all(&inbound_buf[0..n]).await {
                             Ok(_n) => {}
                             Err(e) => {
-                                eprintln!("failed to write to socket; err = {:?}", e);
+                                eprintln!("failed to write to stream; err = {:?}", e);
                                 return;
                             }
                         }

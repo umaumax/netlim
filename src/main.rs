6909e7ee98c3f551f44adc1e26062e97faab8767
umaumax <umauma7083@gmail.com>
2022-02-26 16:51:06 +0900

    Update bandwidth limit features

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::cmp;
-use std::ops::Sub;
+use std::env;
+use std::sync::Arc;
 
-use std::time::{Duration, Instant};
-
+use log::{debug, error, info, warn};
+use structopt::StructOpt;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-
-use structopt::StructOpt;
+use tokio::sync::Mutex;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -14,6 +13,14 @@ struct Cli {
     src_socket: String,
     #[structopt(long = "dst", default_value("localhost:22222"), help = "dst socket")]
     dst_socket: String,
+
+    #[structopt(
+        long = "share",
+        default_value("true"),
+        parse(try_from_str),
+        help = "share bandwidth limit flag"
+    )]
+    share_bandwidth_limit: bool,
 
     #[structopt(
         long = "in",
@@ -27,6 +34,14 @@ struct Cli {
         help = "outbound bandwidth limit [B]"
     )]
     outbound_bandwidth_limit: f64,
+
+    #[structopt(
+        long = "verbose",
+        default_value("false"),
+        parse(try_from_str),
+        help = "verbose flag"
+    )]
+    verbose: bool,
 }
 
 // TODO: add run flag to determine client port and server address
@@ -34,98 +49,105 @@ struct Cli {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
 
+    env::set_var("RUST_LOG", "info");
+    if args.verbose {
+        env::set_var("RUST_LOG", "debug");
+    }
+    env_logger::init();
+
     let listener = TcpListener::bind(args.src_socket).await?;
 
-    let interval_secs = 0.1;
-    let interval_micros = (interval_secs * 1000.0 * 1000.0) as u64;
+    let interval_secs = 0.01;
     let outbound_buffer_size = (args.outbound_bandwidth_limit as f64 * interval_secs) as usize;
     let inbound_buffer_size = (args.inbound_bandwidth_limit as f64 * interval_secs) as usize;
     // TODO: update logs
-    eprintln!("start server {:?}", listener);
+    info!("start server {:?}", listener);
+
+    let next_outbound_time = Arc::new(Mutex::new(tokio::time::Instant::now()));
+    let next_inbound_time = Arc::new(Mutex::new(tokio::time::Instant::now()));
 
     loop {
         let (mut client_stream, _) = listener.accept().await?;
 
         let dst_socket = args.dst_socket.clone();
+
+        let mut next_outbound_time = Arc::clone(&next_outbound_time);
+        let mut next_inbound_time = Arc::clone(&next_inbound_time);
         tokio::spawn(async move {
-            eprintln!("connect from xxx");
+            debug!("connect from xxx");
             let mut server_stream = TcpStream::connect(dst_socket.clone()).await.unwrap();
-            eprintln!("connect to {}", dst_socket.clone());
+            debug!("connect to {}", dst_socket.clone());
 
             let mut outbound_buf = vec![0; outbound_buffer_size];
             let mut inbound_buf = vec![0; inbound_buffer_size];
-            let mut outbound_capacity = outbound_buffer_size;
-            let mut inbound_capacity = inbound_buffer_size;
-            let mut pre_timestamp = Instant::now();
-            // TODO: add close step
-            loop {
-                let timeout_sleep =
-                    tokio::time::sleep(tokio::time::Duration::from_micros(interval_micros));
-                tokio::pin!(timeout_sleep);
 
+            // TODO: add close step
+
+            if !args.share_bandwidth_limit {
+                next_outbound_time = Arc::new(Mutex::new(tokio::time::Instant::now()));
+                next_inbound_time = Arc::new(Mutex::new(tokio::time::Instant::now()));
+            }
+            loop {
+                let now = tokio::time::Instant::now();
                 tokio::select! {
                     // send(outbound)
-                    n = client_stream.read(&mut outbound_buf[0..outbound_capacity]), if outbound_capacity > 0 => {
+                    _ = tokio::time::sleep_until(*next_outbound_time.lock().await), if now < *next_outbound_time.lock().await => {
+                    }
+                    n = client_stream.read(&mut outbound_buf), if now >= *next_outbound_time.lock().await => {
                         let n = match n {
                             Ok(n) if n == 0 => return,
                             Ok(n) => n,
                             Err(e) => {
-                                eprintln!("failed to read from stream; err = {:?}", e);
+                                error!("failed to read from stream; err = {:?}", e);
                                 return;
                             }
                         };
-                        eprintln!("-receive-> {} bytes", n);
+                        debug!("-receive-> {} bytes", n);
 
                         match server_stream.write_all(&outbound_buf[0..n]).await {
                             Ok(_n) => {}
                             Err(e) => {
-                                eprintln!("failed to write to stream; err = {:?}", e);
+                                error!("failed to write to stream; err = {:?}", e);
                                 return;
                             }
                         }
 
-                        outbound_capacity -= n;
-                        outbound_capacity = cmp::max(outbound_capacity, 0);
+                        debug!("-send-> {} bytes", n);
 
-                        eprintln!("-send-> {} bytes", n);
+                        {
+                            let mut next_outbound_time = next_outbound_time.lock().await;
+                            *next_outbound_time = next_outbound_time.max(tokio::time::Instant::now()) + tokio::time::Duration::from_secs_f64(n as f64 / args.outbound_bandwidth_limit as f64);
+                        }
                     }
                     // receive(inbound)
-                    n = server_stream.read(&mut inbound_buf[0..inbound_capacity]), if inbound_capacity > 0 => {
+                    _ = tokio::time::sleep_until(*next_inbound_time.lock().await), if now < *next_inbound_time.lock().await => {
+                    }
+                    n = server_stream.read(&mut inbound_buf), if now >= *next_inbound_time.lock().await => {
                         let n = match n {
                             Ok(n) if n == 0 => return,
                             Ok(n) => n,
                             Err(e) => {
-                                eprintln!("failed to read from stream; err = {:?}", e);
+                                error!("failed to read from stream; err = {:?}", e);
                                 return;
                             }
                         };
-                        eprintln!("<-receive- {} bytes", n);
+                        debug!("<-receive- {} bytes", n);
 
                         match client_stream.write_all(&inbound_buf[0..n]).await {
                             Ok(_n) => {}
                             Err(e) => {
-                                eprintln!("failed to write to stream; err = {:?}", e);
+                                error!("failed to write to stream; err = {:?}", e);
                                 return;
                             }
                         }
-                        inbound_capacity -= n;
-                        inbound_capacity = cmp::max(inbound_capacity, 0);
+                        debug!("<-send- {} bytes", n);
 
-                        eprintln!("<-send- {} bytes", n);
-                    }
-                    _ = &mut timeout_sleep => {
-                        eprintln!("timeout");
+                        {
+                            let mut next_inbound_time = next_inbound_time.lock().await;
+                            *next_inbound_time = next_inbound_time.max(tokio::time::Instant::now()) + tokio::time::Duration::from_secs_f64(n as f64 / args.outbound_bandwidth_limit as f64);
+                        }
                     }
                 };
-                let duration = pre_timestamp.elapsed().as_micros() as u64;
-                if duration >= interval_micros {
-                    // reset capacity limit
-                    eprintln!("reset capacity limit");
-                    let now = Instant::now();
-                    pre_timestamp = now.sub(Duration::from_micros(duration % interval_micros));
-                    outbound_capacity = outbound_buffer_size;
-                    inbound_capacity = inbound_buffer_size;
-                }
             }
         });
     }

b56441fa50cc01fce6231f3c1f55013fe1c1a07f
umaumax <umauma7083@gmail.com>
2022-02-27 17:57:10 +0900

    Refactor main function to NetBandwidthLimitter struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::net::SocketAddr;
 use std::sync::Arc;
 
 use log::{debug, error, info};
@@ -40,50 +41,137 @@ struct Cli {
     verbose: bool,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Cli::from_args();
+pub struct NetBandwidthLimitter {
+    src_socket: String,
+    dst_socket: String,
+    listener: Option<TcpListener>,
+    outbound_bandwidth_limit: u64,
+    inbound_bandwidth_limit: u64,
+    next_outbound_time: Arc<Mutex<tokio::time::Instant>>,
+    next_inbound_time: Arc<Mutex<tokio::time::Instant>>,
+    share_bandwidth_limit: bool,
+}
 
-    env::set_var("RUST_LOG", "info");
-    if args.verbose {
-        env::set_var("RUST_LOG", "debug");
+impl NetBandwidthLimitter {
+    fn try_new(
+        src_socket: impl Into<String>,
+        dst_socket: impl Into<String>,
+        outbound_bandwidth_limit: u64,
+        inbound_bandwidth_limit: u64,
+        share_bandwidth_limit: bool,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let src_socket = src_socket.into();
+        let dst_socket = dst_socket.into();
+        Ok(Self {
+            src_socket,
+            dst_socket,
+            listener: None,
+            outbound_bandwidth_limit,
+            inbound_bandwidth_limit,
+            next_outbound_time: Arc::new(Mutex::new(tokio::time::Instant::now())),
+            next_inbound_time: Arc::new(Mutex::new(tokio::time::Instant::now())),
+            share_bandwidth_limit,
+        })
     }
-    env_logger::init();
 
-    let listener = TcpListener::bind(args.src_socket).await?;
+    async fn bind(&mut self) -> Result<(), std::io::Error> {
+        self.listener = Some(TcpListener::bind(&self.src_socket).await?);
+        Ok(())
+    }
 
-    let interval_secs = 0.1;
-    let outbound_buffer_size = (args.outbound_bandwidth_limit as f64 * interval_secs) as usize;
-    let inbound_buffer_size = (args.inbound_bandwidth_limit as f64 * interval_secs) as usize;
-    info!("started server at {:?}", listener.local_addr()?);
+    async fn accept(&self) -> Result<(TcpStream, SocketAddr), std::io::Error> {
+        self.listener.as_ref().unwrap().accept().await
+    }
 
-    let next_outbound_time = Arc::new(Mutex::new(tokio::time::Instant::now()));
-    let next_inbound_time = Arc::new(Mutex::new(tokio::time::Instant::now()));
+    async fn send_outbound(
+        server_stream: &mut TcpStream,
+        outbound_buf: &mut [u8],
+        next_outbound_time: &Arc<Mutex<tokio::time::Instant>>,
+        outbound_bandwidth_limit: u64,
+        n: Result<usize, tokio::io::Error>,
+    ) -> Result<usize, tokio::io::Error> {
+        let n = match n {
+            Ok(n) if n == 0 => return Ok(0),
+            Ok(n) => n,
+            Err(e) => {
+                return Err(e);
+            }
+        };
+        debug!("[outbound] -recv-> {} bytes", n);
 
-    loop {
-        let (mut client_stream, client_addr) = listener.accept().await?;
+        let _n = server_stream.write_all(&outbound_buf[0..n]).await?;
+        debug!("[outbound] -send-> {} bytes", n);
 
-        let dst_socket = args.dst_socket.clone();
+        let sleep_duration =
+            tokio::time::Duration::from_secs_f64(n as f64 / outbound_bandwidth_limit as f64);
+        {
+            let mut next_outbound_time = next_outbound_time.lock().await;
+            *next_outbound_time =
+                next_outbound_time.max(tokio::time::Instant::now()) + sleep_duration;
+        }
+        Ok(n)
+    }
 
-        let mut next_outbound_time = Arc::clone(&next_outbound_time);
-        let mut next_inbound_time = Arc::clone(&next_inbound_time);
+    async fn recv_inbound(
+        client_stream: &mut TcpStream,
+        inbound_buf: &mut [u8],
+        next_inbound_time: &Arc<Mutex<tokio::time::Instant>>,
+        inbound_bandwidth_limit: u64,
+        n: Result<usize, tokio::io::Error>,
+    ) -> Result<usize, tokio::io::Error> {
+        let n = match n {
+            Ok(n) if n == 0 => return Ok(0),
+            Ok(n) => n,
+            Err(e) => {
+                return Err(e);
+            }
+        };
+        debug!("[inbound] <-recv- {} bytes", n);
+
+        let _n = client_stream.write_all(&inbound_buf[0..n]).await?;
+        debug!("[inbound] <-send- {} bytes", n);
+
+        let sleep_duration =
+            tokio::time::Duration::from_secs_f64(n as f64 / inbound_bandwidth_limit as f64);
+        {
+            let mut next_inbound_time = next_inbound_time.lock().await;
+            *next_inbound_time =
+                next_inbound_time.max(tokio::time::Instant::now()) + sleep_duration;
+        }
+        Ok(n)
+    }
+
+    async fn spawn(
+        &self,
+        (mut client_stream, client_addr): (TcpStream, SocketAddr),
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let mut next_outbound_time = Arc::clone(&self.next_outbound_time);
+        let mut next_inbound_time = Arc::clone(&self.next_inbound_time);
+
+        let interval_secs = 0.1;
+        let outbound_buffer_size = (self.outbound_bandwidth_limit as f64 * interval_secs) as usize;
+        let inbound_buffer_size = (self.inbound_bandwidth_limit as f64 * interval_secs) as usize;
+        let dst_socket = self.dst_socket.clone();
+        let share_bandwidth_limit = self.share_bandwidth_limit;
+        let outbound_bandwidth_limit = self.outbound_bandwidth_limit;
+        let inbound_bandwidth_limit = self.inbound_bandwidth_limit;
         tokio::spawn(async move {
             info!("connected from {:?}", client_addr);
-            let mut server_stream = match TcpStream::connect(dst_socket.clone()).await {
+            let mut server_stream = match TcpStream::connect(&dst_socket).await {
                 Ok(s) => s,
                 Err(e) => {
                     error!("failed to connect to dst server; err = {:?}", e);
                     return;
                 }
             };
-            info!("connected to dst server at {}", dst_socket.clone());
+            info!("connected to dst server at {}", &dst_socket);
 
             let mut outbound_buf = vec![0; outbound_buffer_size];
             let mut inbound_buf = vec![0; inbound_buffer_size];
 
             // TODO: add close step
 
-            if !args.share_bandwidth_limit {
+            if !share_bandwidth_limit {
                 next_outbound_time = Arc::new(Mutex::new(tokio::time::Instant::now()));
                 next_inbound_time = Arc::new(Mutex::new(tokio::time::Instant::now()));
             }
@@ -94,63 +182,61 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     _ = tokio::time::sleep_until(*next_outbound_time.lock().await), if now < *next_outbound_time.lock().await => {
                     }
                     n = client_stream.read(&mut outbound_buf), if now >= *next_outbound_time.lock().await => {
-                        let n = match n {
-                            Ok(n) if n == 0 => return,
-                            Ok(n) => n,
+                        match NetBandwidthLimitter::send_outbound(&mut server_stream, &mut outbound_buf,&next_outbound_time,outbound_bandwidth_limit,n).await {
+                            Ok(0) => break,
+                            Ok(_) => {},
                             Err(e) => {
                                 error!("failed to read from stream; err = {:?}", e);
-                                return;
+                                break
                             }
                         };
-                        debug!("[inbound] -recv-> {} bytes", n);
-
-                        match server_stream.write_all(&outbound_buf[0..n]).await {
-                            Ok(_n) => {}
-                            Err(e) => {
-                                error!("failed to write to stream; err = {:?}", e);
-                                return;
-                            }
-                        }
-
-                        debug!("[inbound] -send-> {} bytes", n);
-
-                        let sleep_duration = tokio::time::Duration::from_secs_f64(n as f64 / args.outbound_bandwidth_limit as f64);
-                        {
-                            let mut next_outbound_time = next_outbound_time.lock().await;
-                            *next_outbound_time = next_outbound_time.max(tokio::time::Instant::now()) + sleep_duration;
-                        }
                     }
                     // receive(inbound)
                     _ = tokio::time::sleep_until(*next_inbound_time.lock().await), if now < *next_inbound_time.lock().await => {
                     }
                     n = server_stream.read(&mut inbound_buf), if now >= *next_inbound_time.lock().await => {
-                        let n = match n {
-                            Ok(n) if n == 0 => return,
-                            Ok(n) => n,
+                        match NetBandwidthLimitter::recv_inbound(&mut client_stream, &mut inbound_buf, &next_inbound_time, inbound_bandwidth_limit, n).await {
+                            Ok(0) => break,
+                            Ok(_) => {},
                             Err(e) => {
                                 error!("failed to read from stream; err = {:?}", e);
-                                return;
+                                break
                             }
                         };
-                        debug!("[outbound] <-recv- {} bytes", n);
-
-                        match client_stream.write_all(&inbound_buf[0..n]).await {
-                            Ok(_n) => {}
-                            Err(e) => {
-                                error!("failed to write to stream; err = {:?}", e);
-                                return;
-                            }
-                        }
-                        debug!("[outbound] <-send- {} bytes", n);
-
-                        let sleep_duration = tokio::time::Duration::from_secs_f64(n as f64 / args.inbound_bandwidth_limit as f64);
-                        {
-                            let mut next_inbound_time = next_inbound_time.lock().await;
-                            *next_inbound_time = next_inbound_time.max(tokio::time::Instant::now()) + sleep_duration;
-                        }
                     }
                 };
             }
         });
+        Ok(())
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::from_args();
+
+    env::set_var("RUST_LOG", "info");
+    if args.verbose {
+        env::set_var("RUST_LOG", "debug");
+    }
+    env_logger::init();
+
+    let mut net_bandwidth_limitter = NetBandwidthLimitter::try_new(
+        &args.src_socket,
+        &args.dst_socket,
+        args.outbound_bandwidth_limit,
+        args.inbound_bandwidth_limit,
+        args.share_bandwidth_limit,
+    )?;
+
+    net_bandwidth_limitter.bind().await?;
+    info!("started server at {:?}", &args.src_socket);
+
+    loop {
+        let (client_stream, client_addr) = net_bandwidth_limitter.accept().await?;
+        net_bandwidth_limitter
+            .spawn((client_stream, client_addr))
+            .await?;
+    }
+    // NOTE: unreachable here
 }
